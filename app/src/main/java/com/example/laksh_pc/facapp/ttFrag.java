96a9a22010adc09fc964a44a0004d7bd602efcc8
rutanshuj <rjhaveri41@gmail.com>
2017-04-13 13:11:42 +0530

    TTfRAG

---
+++ app/src/main/java/com/example/laksh_pc/facapp/ttFrag.java
@@ -0,0 +1,9 @@
+package com.example.laksh_pc.facapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ttFrag extends Fragment {
+
+}
